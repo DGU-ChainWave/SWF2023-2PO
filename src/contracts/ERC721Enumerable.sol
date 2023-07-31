d4de21a73d018559ae29526e2b813d021040cba5
Kong-E <kng001016@dgu.ac.kr>
2023-08-01 07:11:17 +0900

    feat: 인터페이스 상속 추가(ERC165)

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import './ERC721.sol';
+import './interfaces/IERC721Enumerable.sol';
 
-contract ERC721Enumerable is ERC721{
+contract ERC721Enumerable is ERC721, IERC721Enumerable {
 
   uint256[] private _allTokens;
 
@@ -15,6 +16,11 @@ contract ERC721Enumerable is ERC721{
 
   // mapping from token ID index of the owner token ids
   mapping(uint256 => uint256) private _ownedTokensIndex;
+
+    constructor() {
+      _registerInterface(bytes4(keccak256('totalSupply(bytes4)')^
+      keccak256('tokenByIndex(bytes4)')));
+  }
   
   function _mint(address to, uint256 tokenId) internal override(ERC721) {
     super._mint(to, tokenId);
@@ -32,17 +38,17 @@ contract ERC721Enumerable is ERC721{
     _ownedTokens[to].push(tokenId);
   } // 토큰을 오너목록에 추가
 
-  function tokenByIndex(uint256 index) public view returns (uint256) {
+  function tokenByIndex(uint256 index) public view override returns (uint256) {
     require(index < totalSupply(), "ERC721Enumerable: global index out of bounds");
     return _allTokens[index];
   } // 인덱스에 해당하는 토큰 반환(토큰 검색)
 
-  function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
+  function tokenOfOwnerByIndex(address owner, uint256 index) public view override returns (uint256) {
     require(index < balanceOf(owner), "ERC721Enumerable: owner index out of bounds");
     return _ownedTokens[owner][index];
   } // 오너의 토큰목록에서 인덱스에 해당하는 토큰 반환(토큰 검색)
 
-  function totalSupply() public view returns (uint256) {
+  function totalSupply() public view override returns (uint256) {
     return _allTokens.length;
   } // 토탈서플라이 길이 반환
 }
